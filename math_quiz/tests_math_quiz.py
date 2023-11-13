06f024d99170c5d84a3775864173ce823e090994
NIRANJAN BALACHANDAR <niranjanbalachandar1996@gmail.com>
2023-11-13 15:59:56 +0100

    Unit test in tests_math_quiz.py

--- math_quiz/tests_math_quiz.py
+++ math_quiz/tests_math_quiz.py
@@ -13,18 +13,23 @@ class TestMathGame(unittest.TestCase):
             self.assertTrue(min_val <= rand_num <= max_val)
 
     def test_function_B(self):
-        # TODO
-        pass
+        operators = {'+','-','*'}
+        for _ in range(1000):
+            result = function_B()
+            self.assertIn(result, operators)
 
     def test_function_C(self):
             test_cases = [
-                (5, 2, '+', '5 + 2', 7),
-                ''' TODO add more test cases here '''
+                (4, 2, '+', '4 + 2', 6),
+                (6, 3, '-', '6 - 3', 3),
+                (9, 2, '*', '9* 2', 18)
+                
             ]
 
             for num1, num2, operator, expected_problem, expected_answer in test_cases:
-                # TODO
-                pass
+                PROBLEM, ANSWER = function_C(num1, num2, operator)
+                self.assertEqual(PROBLEM, expected_problem) 
+                self.assertEqual(ANSWER, expected_answer)
 
 if __name__ == "__main__":
     unittest.main()
