a9602916a7554c1830f4d537160979a9280439b5
Ninjack96 <150633422+Ninjack96@users.noreply.github.com>
2023-11-13 16:22:41 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="math_quiz",
+    version="0.1.0",
+    description="Test your Math quiz using PIP tool",
+    author="N",
+    author_email="niranjanbalachandar1996@gmail.com",
+    url="https://github.com/Ninjack96/DSSS_HW_01_Niranjan",
+    packages=["math_quiz"],
+    install_requires=[],
+)
